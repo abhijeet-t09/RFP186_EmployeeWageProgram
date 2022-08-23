565cefcd2827cc0dfe8e766646f8d6442e7f52b2
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-23 14:54:31 +0530

    Employee Daily Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,15 +3,26 @@ package com.bridgelabz;
 public class EmployeeWage {
 
     static final int IS_PRESENT = 1;
+    static final int RATE_PER_HOUR = 20;
+
     public static void main(String[] args) {
         System.out.println("**** Employee Wage Computation Program ****\n\n");
 
-        int checkAttendance = (int)Math.floor(Math.random()*10)%2;
+        int empHours = 0;
+        int empWage = 0;
 
-        if(checkAttendance == IS_PRESENT)
+        int checkAttendance = (int)Math.floor(Math.random() * 10) % 2;
+
+        if(checkAttendance == IS_PRESENT) {
             System.out.println("Employee is Present!!");
-        else
+            empHours = 8;
+        }
+        else {
             System.out.println("Employee is Absent!!");
+            empHours = 0;
+        }
 
+        empWage = empHours * RATE_PER_HOUR;
+        System.out.println("Employee Wage = " + empWage);
     }
 }
