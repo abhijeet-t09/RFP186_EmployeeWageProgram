419d01619c572521094030dca66c2a88334d4aea
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-23 18:11:52 +0530

    Employee Monthly Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,45 +5,36 @@ public class EmployeeWage {
     static final int IS_PART_TIME = 1;
     static final int IS_FULL_TIME = 2;
     static final int RATE_PER_HOUR = 20;
+    static final int TOTAL_WORKING_DAYS = 20;
 
     public static void main(String[] args) {
         System.out.println("**** Employee Wage Computation Program ****\n\n");
 
         int empHours = 0;
         int empWage = 0;
+        int totalWage = 0;
 
-        int checkAttendance = (int) Math.floor(Math.random() * 10) % 3;
+        for(int i = 1; i <= TOTAL_WORKING_DAYS; i++){
 
-//        if (checkAttendance == IS_PART_TIME) {
-//            System.out.println("Employee Works Part-time");
-//            empHours = 5;
-//        }
-//        else if (checkAttendance == IS_FULL_TIME) {
-//            System.out.println("Employee Works Full-time");
-//            empHours = 8;
-//        }
-//        else {
-//            System.out.println("Employee is Absent!!");
-//            empHours = 0;
-//        }
+            int checkAttendance = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch (checkAttendance){
-            case IS_PART_TIME:
-                System.out.println("Employee Works Part-time");
-                empHours = 5;
-                break;
+            switch (checkAttendance){
+                case IS_PART_TIME:
+                    empHours = 5;
+                    break;
 
-            case IS_FULL_TIME:
-                System.out.println("Employee Works Full-time");
-                empHours = 8;
-                break;
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
 
-            default:
-                empHours = 0;
+                default:
+                    empHours = 0;
+            }
+            empWage = empHours * RATE_PER_HOUR;
+            System.out.println("Employee Wage for Day " + i + " = " + empWage);
+            totalWage = totalWage + empWage;
         }
-
-        empWage = empHours * RATE_PER_HOUR;
-        System.out.println("Employee Wage = " + empWage);
+        System.out.println("\nEmployee Monthly Wage = " +totalWage);
     }
 }
 
